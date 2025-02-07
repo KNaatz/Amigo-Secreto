45abf9ee6bef06371ad2764bde1a3a5f609ddaf0
Kauany Naatz <103617314+KNaatz@users.noreply.github.com>
2025-02-07 14:28:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return; 
+    }
+
+amigos.push(nome);
+input.value = "";
+atualizarLista();
+
+console.log(amigos);
+}
+
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Não há amigos para sortear!");
+        return;
+    };
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice];
+    document.getElementById("resultado").innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+};
